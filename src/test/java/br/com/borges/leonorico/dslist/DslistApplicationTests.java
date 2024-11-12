c6e867072e4f1f37e5c30d0e5c4c2ff227e4658b
Leonorico <leonoricoeduardo.borges@estudante.ufjf.br>
2024-11-12 14:56:07 -0300

    Projeto criado

---
+++ src/test/java/br/com/borges/leonorico/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.borges.leonorico.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
